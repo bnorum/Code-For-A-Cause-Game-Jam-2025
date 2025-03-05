dc8f35b09a8918a22a17f607b9fb28bb4f76a1f1
Brady Norum <bnorum1@pride.hofstra.edu>
2025-03-05 14:53:28 -0500

    more orientation stuff

--- Assets/OrientationManager.cs
+++ Assets/OrientationManager.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
@@ -11,6 +12,7 @@ public class OrientationManager : MonoBehaviour
     public VideoPlayer videoPlayer;
     public TextMeshProUGUI associatedDetailsText;
     public int index = 0;
+    public float screenShakeTimer = 0.0f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -21,6 +23,12 @@ public class OrientationManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        screenShakeTimer -= Time.deltaTime;
+        if (screenShakeTimer < 0.0f)
+        {
+            StartCoroutine(ScreenShake());
+            screenShakeTimer = Random.Range(15,20);
+        }
         associatedDetailsText.text = associatedDetails[index];
     }
 
@@ -38,5 +46,24 @@ public class OrientationManager : MonoBehaviour
         index = (index + 1) % videos.Count;
         videoPlayer.clip = videos[index];
         videoPlayer.Play();
+    }
+
+    public IEnumerator ScreenShake() {
+        Vector3 originalPosition = videoPlayer.transform.position;
+        float elapsed = 0.0f;
+
+        while (elapsed < 0.1f)
+        {
+            float x = Random.Range(-0.05f, 0.05f);
+            float y = Random.Range(-0.05f, 0.05f);
+
+            videoPlayer.transform.position = new Vector3(originalPosition.x + x, originalPosition.y + y, originalPosition.z);
+
+            elapsed += 0.05f;
+
+            yield return new WaitForSeconds(0.05f);
+        }
+
+        videoPlayer.transform.position = originalPosition;
     }
 }
